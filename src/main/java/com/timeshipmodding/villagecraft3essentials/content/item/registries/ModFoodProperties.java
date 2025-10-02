cb6a4e097b6d0e7605d5dd90287ebf74f2168f14
TardisEngineer <110013308+TardisEngineer@users.noreply.github.com>
2025-10-02 11:31:15 +1000

    Rebalance worm hunger and spawn rates

--- src/main/java/com/timeshipmodding/villagecraft3essentials/content/item/registries/ModFoodProperties.java
+++ src/main/java/com/timeshipmodding/villagecraft3essentials/content/item/registries/ModFoodProperties.java
@@ -5,6 +5,6 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties WORM = new FoodProperties.Builder().nutrition(4).saturationModifier(0.1f).effect(() -> new MobEffectInstance(MobEffects.POISON, 200, 0), 0.8f).build();
-    public static final FoodProperties COOKED_WORM = new FoodProperties.Builder().nutrition(3).saturationModifier(0.4f).build();
+    public static final FoodProperties WORM = new FoodProperties.Builder().nutrition(1).saturationModifier(0.05f).effect(() -> new MobEffectInstance(MobEffects.POISON, 200, 0), 0.8f).build();
+    public static final FoodProperties COOKED_WORM = new FoodProperties.Builder().nutrition(3).saturationModifier(0.2f).build();
 }
